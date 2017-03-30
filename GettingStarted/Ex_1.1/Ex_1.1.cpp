f314a5ac07b81699cd2aa1309cda3538def2bda0
Ankit Mishra <ankit1703@gmail.com>
2017-03-30 09:57:15 +0530

    Initial Commit

---
+++ GettingStarted/Ex_1.1/Ex_1.1.cpp
@@ -0,0 +1,15 @@
+// Ex_1.1.cpp : 
+//
+
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "Hello World !!" << endl;
+	return 0;
+}
+
+
+
---
+++ GettingStarted/Ex_1.2/Ex_1.2.cpp
@@ -0,0 +1,13 @@
+// Ex_1.2.cpp : Defines the entry point for the console application.
+// 
+
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    return -1;
+}
+
+// No changes occured while returning -1
