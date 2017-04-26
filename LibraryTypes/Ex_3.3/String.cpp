644e611bb239f225180815af4c1b457e010de29c
Ankit Mishra <ankit1703@gmail.com>
2017-04-26 14:25:13 +0530

    Initial Commit

---
+++ LibraryTypes/Ex_3.3/String.cpp
@@ -0,0 +1,14 @@
+// 3 Ways to initialize a string
+
+#include <string>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	string s1;
+	string s2(s1);
+	string s3("Hello World ");
+
+	return 0;
+}
